75036ba1488babe2e5cb78b1c5cda8867250cc5f
EthanTheGrate <amogussussy1221@outlook.com>
2023-12-16 09:57:49 -0700

    holy moly fyodor nnghh mmph

--- turtle_test.py
+++ turtle_test.py
@@ -6,9 +6,11 @@ turtle.speed(500)
 turtle.bgcolor("black")
 turtle.pensize(1)
 
-for i in range(1000):
-    turtle.color(colors[i % len(colors)])
-    turtle.forward(35)
-    turtle.left(13)
+for i in range(2):
+    for i in range():
+        turtle.color(colors[i % len(colors)])
+        turtle.forward(10)
+        turtle.left(9)
+    turtle.forward(30)
 
 turtle.done()
