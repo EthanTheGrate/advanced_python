c7daff8e4a12c4a06ee33851b3e2fcd41fcf6469
EthanTheGrate <amogussussy1221@outlook.com>
2024-04-27 10:07:57 -0600

    bell noises young fly on the track

---
+++ question.py
@@ -0,0 +1,40 @@
+# givenstring = "3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 39 42 43 44 45 46"
+# placestring = ""
+# holdvar = 0
+
+# cum = givenstring.split()
+# print(cum)
+# for i in range(len(cum)):
+#     holdvar = cum[i]
+#     if int(holdvar) % 3 == 0 and int(holdvar) % 5 == 0:
+#         placestring += (holdvar)
+#         placestring += (" ")
+# print(placestring)
+
+
+
+# listoffruits = ["cucumber", "eggplant", "corn", "cauliflower", "zucchini", "asparagus"]
+# end = []
+# first = "h"
+
+# for i in range(len(listoffruits)):
+#     first = listoffruits[i][0]
+#     if first == "a" or first == "m":
+#         if len(listoffruits[i]) > 5:
+#             end.append(listoffruits[i])
+# print(end)
+
+
+
+# get = int(input("gimme yo money\n"))
+
+# for i in range(12):
+#     print(get, "x", i, "=", get * i)
+
+
+
+# string = "GETOUTOFMYHEADN"
+
+# string = string.lower()
+# string = string.replace("n", "Ethan")
+# print(string)
